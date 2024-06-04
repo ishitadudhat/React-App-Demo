121df7c42c64a6407761d17bedcd0f1216d31a57
ishitadudhat <ishitadudhat2501@gmail.com>
2024-06-04 18:39:36 +0530

    Add Bootstrap

---
+++ module 2.mjs
@@ -0,0 +1,9 @@
+const a = "Dhaval";
+const b = "Prince";
+const c = "Parth";
+const d = "Dhruv";
+
+export default a;
+export {b};
+export {c};
+export {d};
--- src/App.js
+++ src/App.js
@@ -1,28 +1,34 @@
 import './App.css';
-import img1 from './images/images.jpg';
 
 function App() {
   return (
     <>
-    <header>
-      <nav>
-        <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Gallery</li>
-          <li>Contact Us</li>
-          <li>Sign Up</li>
-          <li>Log in</li>
-        </ul>
-      </nav>
-
-      <p className="lorem-p">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Similique, repellat officiis laboriosam quaerat ut aperiam consequuntur doloremque dolorem magni assumenda laborum ad ex eos provident dolorum id neque est animi? Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate nisi ipsa est quidem obcaecati numquam facilis inventore officia at consectetur, aperiam voluptatem impedit itaque nam vel debitis. Saepe, nostrum rem! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error reiciendis et dolorem eius commodi temporibus rem iste explicabo eaque quisquam nostrum ab, cupiditate iure ratione totam harum perspiciatis praesentium labore! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Soluta, omnis sint officia voluptate assumenda provident, laborum expedita consequuntur temporibus quos deserunt excepturi incidunt consectetur fuga eum adipisci alias eius perferendis.</p>
-
-      <img className="img2" src={img1} alt="" />
-
-      <img className="img1" src="https://thumbs.dreamstime.com/z/mt-fuji-420-4612948.jpg" alt="" />
-
-    </header>
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="#">SIT</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Link</a>
+        </li>
+        
+        <li className="nav-item">
+          <a className="nav-link disabled" aria-disabled="true">Disabled</a>
+        </li>
+      </ul>
+      <form className="d-flex" role="search">
+        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+        <button className="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
     </>
   );
 }
