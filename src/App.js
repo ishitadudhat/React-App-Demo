edff8a0cdb3e2762f330aa63d635d91af9e63d68
ishitadudhat <ishitadudhat2501@gmail.com>
2024-06-06 19:18:18 +0530

    whole website is done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
-
+import Navbar from './components/Navbar'
+import TextForm from './components/TextForm'
 
 function App() {
   return (
     <>
-    
+    <Navbar title="SIT" aboutText="Contact Us"/>
+    <TextForm heading="Enter Text to Analyse Below"/>
     </>
   );
 }
---
+++ src/components/TextForm.js
@@ -0,0 +1,57 @@
+import React,{useState} from 'react'
+
+export default function TextForm(props) {
+    const [text, setText]= useState("Input Your Text");
+    // setText("Enter Your Text"); 
+
+    const handleOnChange = (event) => {
+        // console.log("On change");
+        setText(event.target.value);
+    }
+
+    const handleUpChange = () => {
+        // console.log("Btn click");
+        let newText = text.toUpperCase();
+        setText(newText);
+    }
+
+    const handleLoChange = () => {
+        // console.log("Btn click");
+        let newText = text.toLowerCase();
+        setText(newText);
+    }
+    const handleSpeak = () => {
+        let newText = new SpeechSynthesisUtterance();
+        newText.text=text;
+        window.speechSynthesis.speak(newText);
+    }
+
+    const handleClear = () => {
+        setText('');
+    }
+    
+    return (
+        <div >
+            <div class="mx-5 my-5 container">
+                <h1>{props.heading}</h1>
+                <textarea className="form-control" id="exampleFormControlTextarea1" rows="8" value={text} onChange={handleOnChange}></textarea>
+                <button className='btn btn-dark my-4 mx-4' onClick={handleUpChange}>Convert to Upper Case</button>
+                <button className='btn btn-outline-dark my-4 mx-4' onClick={handleLoChange}>Convert to Lower Case</button>
+                <button className='btn btn-dark my-4 mx-4' onClick={handleSpeak}>Speak</button>
+                <button className='btn btn-outline-dark my-4 mx-4' onClick={handleClear}>Clear</button>
+            </div>
+
+            <div className='container'>
+                <h2>Words Count</h2>
+                <p>{text.split(' ').length} Words </p>
+                <p>{text.length} Characters </p>
+                <p>{0.008*text.length} Reading Time</p>
+                <h3>Preview</h3>
+                <p>{text}</p>
+
+
+
+            </div>
+        </div>
+    )
+}
