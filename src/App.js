dbb53814096264b5c5dd0e690543a6c13d71468d
ishitadudhat <ishitadudhat2501@gmail.com>
2024-06-06 18:00:03 +0530

    hi

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
-import Navbar from './components/Navbar.js'
-import TextForm from './components/TextForm.js'
+
 
 function App() {
   return (
     <>
-    <Navbar title="SIT" aboutText="Contact Us"/>
-    <TextForm heading="Enter Text to Analyse Below" />
+    
     </>
   );
 }
