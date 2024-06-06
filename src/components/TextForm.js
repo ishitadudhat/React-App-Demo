0fcb9938f92be8e1db787d22725ba59b310b8674
ishitadudhat <ishitadudhat2501@gmail.com>
2024-06-06 16:44:24 +0530

    Add Upper Text Function

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,15 +1,26 @@
 import React,{useState} from 'react'
 
 export default function TextForm(props) {
-    var [text, setText]= useState("Input Your Text");
-    // setText=("Enter Your Text");
+    const [text, setText]= useState("Input Your Text");
+    // setText("Enter Your Text"); 
+
+    const handleOnChange = (event) => {
+        // console.log("On change");
+        setText(event.target.value);
+    }
+
+    const handleUpChange = () => {
+        // console.log("Btn click");
+        let newText = text.toUpperCase();
+        setText(newText);
+    }
     
     return (
         <div >
             <div class="mx-5 my-5">
                 <h1>{props.heading}</h1>
-                <textarea className="form-control" id="exampleFormControlTextarea1" rows="8" value={text}></textarea>
-                <button className='btn btn-success my-4'>Convert to Upper Case</button>
+                <textarea className="form-control" id="exampleFormControlTextarea1" rows="8" value={text} onChange={handleOnChange}></textarea>
+                <button className='btn btn-success my-4' onClick={handleUpChange}>Convert to Upper Case</button>
             </div>
         </div>
     )
