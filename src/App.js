9a0cef9d5f084aa14561e91ffe013f7e61831516
ishitadudhat <ishitadudhat2501@gmail.com>
2024-06-08 17:15:24 +0530

    add half mode change

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
+import { useState } from 'react';
 import './App.css';
 import Navbar from './components/Navbar.js'
 import TextForm from './components/TextForm.js'
 
 function App() {
+
+  const [mode, setMode] = useState('light')
+
+  const toggleMode = () => {
+    if (mode === 'light') {
+      setMode('dark');
+      document.body.style.background= 'black'
+    } else {
+      setMode('light');
+      document.body.style.background= 'white'
+    }
+  }
+  
   return (
     <>
-    <Navbar title="SIT" aboutText="Contact Us"/>
+    <Navbar title="SIT" aboutText="Contact Us" mode={mode} toggleMode = {toggleMode}/>
     <TextForm heading="Enter Text to Analyse Below" />
     </>
   );
